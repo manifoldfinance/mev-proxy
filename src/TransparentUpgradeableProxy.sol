2206e77e2304136bd9db44ba8b2f9d0fb02877c9
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2023-06-21 21:59:42 -0600

    locked in version for non PUSH0 chains

--- src/TransparentUpgradeableProxy.sol
+++ src/TransparentUpgradeableProxy.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.9.0) (proxy/transparent/TransparentUpgradeableProxy.sol)
 
-pragma solidity ^0.8.19;
+pragma solidity 0.8.19;
 
 import "./ERC1967Proxy.sol";
 import "./interfaces/IERC1967.sol";
